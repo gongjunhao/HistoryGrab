9242d421b151ce7db8b6a9353d3073e7061bdef1
gongjunhao <gongjunhao@sina.com>
2015-10-04 16:00:48 +0800

    &

--- utils/src/main/java/com/historygrab/utils/googlemap/GoogleMapsUtils.java
+++ utils/src/main/java/com/historygrab/utils/googlemap/GoogleMapsUtils.java
@@ -1,7 +1,9 @@
 package com.historygrab.utils.googlemap;
 
 /**
- * Created by Junhao on 2015/10/1.
+ * Created by Junhao on 2015/10/1.20:28
+ *
+ * @version 1.0
  */
 public class GoogleMapsUtils {
     public static void main(String args[]) {
